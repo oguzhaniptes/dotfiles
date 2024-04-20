f4299cf05fc30043659c60cd5f189046905821c0
oguzhaniptes <oguzhaniptes02@gmail.com>
2024-04-20 20:18:05 +0300

    fix unknow monitor

--- hypr/scripts/monitor-switcher.sh
+++ hypr/scripts/monitor-switcher.sh
@@ -7,7 +7,7 @@ if grep open /proc/acpi/button/lid/LID0/state; then
         hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 1920x-720, 1"
     else
         hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
-        hyprctl keyword monitor "HDMI-A-1, preferred, 1920x-720, 1"
+        hyprctl keyword monitor "HDMI-A-1, preferred, auto, 1"
     fi
 else
     if [[ $(hyprctl monitors | grep "Monitor" | wc -l) != 1 ]]; then
@@ -16,7 +16,7 @@ else
             hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 0x0, 1"
         else
             hyprctl keyword monitor "eDP-1, disable"
-            hyprctl keyword monitor "HDMI-A-1, preferred, 1920x-720, 1"
+            hyprctl keyword monitor "HDMI-A-1, preferred, auto, 1"
         fi
     fi
 fi

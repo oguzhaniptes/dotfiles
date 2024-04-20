2d41234fed97ffdd5408ad35187d0f671c5d18be
oguzhaniptes <oguzhaniptes02@gmail.com>
2024-04-20 20:15:07 +0300

    lid monitor switch fixed

--- hypr/scripts/monitor-switcher.sh
+++ hypr/scripts/monitor-switcher.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
+TAB=$'\t'
 
 if grep open /proc/acpi/button/lid/LID0/state; then
-    hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
-    hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 1920x-720, 1"
+    if [[ $(hyprctl monitors | grep "model" | awk "NR==1") == "${TAB}model: DELL S2721DS" ]]; then
+        hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
+        hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 1920x-720, 1"
+    else
+        hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
+        hyprctl keyword monitor "HDMI-A-1, preferred, 1920x-720, 1"
+    fi
 else
-    if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
-        hyprctl keyword monitor "eDP-1, disable"
-        hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 0x0, 1"
+    if [[ $(hyprctl monitors | grep "Monitor" | wc -l) != 1 ]]; then
+        if [[ $(hyprctl monitors | grep "model" | awk "NR==1") == "${TAB}model: DELL S2721DS" ]]; then
+            hyprctl keyword monitor "eDP-1, disable"
+            hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 0x0, 1"
+        else
+            hyprctl keyword monitor "eDP-1, disable"
+            hyprctl keyword monitor "HDMI-A-1, preferred, 1920x-720, 1"
+        fi
     fi
 fi

2353942ec98d333bfc96ee1ed36664bc26b98fd7
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-09-26 22:56:27 +0300

    update monitor switcher script

--- hypr/scripts/monitor-switcher.sh
+++ hypr/scripts/monitor-switcher.sh
@@ -1,22 +1,32 @@
 #!/usr/bin/env bash
 TAB=$'\t'
+MY_INTERNAL_DISPLAY_MODEL="0x004D"
+MY_MONITOR_MODEL="DELL S2721DS"
 
 if grep open /proc/acpi/button/lid/LID0/state; then
-    if [[ $(hyprctl monitors | grep "model" | awk "NR==1") == "${TAB}model: DELL S2721DS" ]]; then
-        hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
-        hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 1920x-720, 1"
+  if [[ $(hyprctl monitors | grep "Monitor" | wc -l) == 1 ]]; then
+    if [[ $(hyprctl monitors | grep "${TAB}model: ${MY_INTERNAL_DISPLAY_MODEL}") ]]; then
+      hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
+    elif [[ $(hyprctl monitors | grep "${TAB}model: ${MY_MONITOR_MODEL}") ]]; then
+      hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 0x0, 1"
     else
-        hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
-        hyprctl keyword monitor "HDMI-A-1, preferred, auto, 1"
+      hyprctl keyword monitor "HDMI-A-1, preferred, auto, 1"
     fi
+  else
+    if [[ $(hyprctl monitors | grep "${TAB}model: ${MY_INTERNAL_DISPLAY_MODEL}") ]] && [[ $(hyprctl monitors | grep "${TAB}model: ${MY_MONITOR_MODEL}") ]]; then
+      hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
+      hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 1920x0, 1"
+    else
+      hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
+      hyprctl keyword monitor "HDMI-A-1, preferred, auto, 1"
+    fi
+  fi
 else
-    if [[ $(hyprctl monitors | grep "Monitor" | wc -l) != 1 ]]; then
-        if [[ $(hyprctl monitors | grep "model" | awk "NR==1") == "${TAB}model: DELL S2721DS" ]]; then
-            hyprctl keyword monitor "eDP-1, disable"
-            hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 0x0, 1"
-        else
-            hyprctl keyword monitor "eDP-1, disable"
-            hyprctl keyword monitor "HDMI-A-1, preferred, auto, 1"
-        fi
-    fi
+  if [[ $(hyprctl monitors | grep "${TAB}model: DELL S2721DS") ]]; then
+    # hyprctl keyword monitor "eDP-1, disable"
+    hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 0x0, 1"
+  else
+    # hyprctl keyword monitor "eDP-1, disable"
+    hyprctl keyword monitor "HDMI-A-1, preferred, auto, 1"
+  fi
 fi

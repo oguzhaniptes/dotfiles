391caeba573732bb6247ea17a7356eae77f9b03f
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-10-26 16:57:27 +0300

    fixed bash command

--- hypr/scripts/monitor-switcher.sh
+++ hypr/scripts/monitor-switcher.sh
@@ -4,7 +4,7 @@ MY_INTERNAL_DISPLAY_MODEL="0x004D"
 MY_MONITOR_MODEL="DELL S2721DS"
 
 if grep open /proc/acpi/button/lid/LID0/state; then
-  hyprctl keyword monitor eDP-1,enable
+  hyprctl keyword monitor eDP-1, enable
   if [[ $(hyprctl monitors | grep "Monitor" | wc -l) == 1 ]]; then
     if [[ $(hyprctl monitors | grep "${TAB}model: ${MY_INTERNAL_DISPLAY_MODEL}") ]]; then
       hyprctl keyword monitor "eDP-1, 1920x1080@144, 0x0, 1"
@@ -23,11 +23,10 @@ if grep open /proc/acpi/button/lid/LID0/state; then
     fi
   fi
 else
+  hyprctl keyword monitor eDP-1, disable
   if [[ $(hyprctl monitors | grep "${TAB}model: ${MY_MONITOR_MODEL}") ]]; then
-    hyprctl keyword monitor "eDP-1, disable"
     hyprctl keyword monitor "HDMI-A-1, 2560x1440@75, 0x0, 1"
   else
-    hyprctl keyword monitor "eDP-1, disable"
     hyprctl keyword monitor "HDMI-A-1, preferred, auto, 1"
   fi
 fi

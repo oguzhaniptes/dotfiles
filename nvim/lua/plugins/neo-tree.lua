0b588d33b13f2a6dd4e71573b86a5147a49569d3
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-10-26 18:40:23 +0300

    fix neotree error

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,10 +1,19 @@
-require("neo-tree").setup({
-  filesystem = {
-    filtered_items = {
-      visible = true,
-      hide_dotfiles = false,
-      hide_gitignored = true,
-    },
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
   },
-})
-
+  config = function()
+      require("neo-tree").setup({
+          filesystem = {
+              filtered_items = {
+                  visible = true,
+                  hide_dotfiles = false,
+                  hide_gitignored = true,
+              },
+              follow_current_file = true,
+          },
+      })
+  end,
+}

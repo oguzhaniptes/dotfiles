67a248b51856c112955b8632a0a09e85e2a20077
Oğuzhan İpteş <oguzhaniptes02@gmail.com>
2025-11-27 14:26:13 +0300

    Add configuration files and update settings for Hyprland and Neovim

--- nvim/lua/config/notify.lua
+++ nvim/lua/config/notify.lua
@@ -1,3 +1,10 @@
-require("notify").setup({
-  background_colour = "#000000",
-})
+return {
+  "rcarriga/nvim-notify",
+  config = function()
+    require("notify").setup({
+      background_colour = "#000000",
+      timeout = 3000,
+    })
+    vim.notify = require("notify")
+  end,
+}
